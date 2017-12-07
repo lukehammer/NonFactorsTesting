2eb447a938d97452c510d738dfee86031ca003ba
Luke Hammer <luke@lukehammermagic.com>
2017-12-07 00:06:38 -0800

    CORRECTION AFTER  move registration to inforecast type

--- NonFactorsTesting/Models/GridFilters.cs
+++ NonFactorsTesting/Models/GridFilters.cs
@@ -1,26 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using NonFactors.Mvc.Grid;
 using System.Linq.Expressions;
-
+using NonFactors.Mvc.Grid;
 
 namespace NonFactorsTesting.Models
 {
-
     public class InForeCastTypeEqual : BaseGridFilter
     {
         public override Expression Apply(Expression expression)
         {
-            return Expression.Equal(expression, Expression.Constant(null));
-
+            return Expression.Equal(expression,
+                Expression.Constant((InForeCastType)Enum.ToObject(typeof(InForeCastType), Value)));
         }
-
-
-}
-
-
-
-
+    }
 }
